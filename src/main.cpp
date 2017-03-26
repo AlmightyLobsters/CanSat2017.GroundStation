5150eea3963c784a1d240b1ff50f431b7b6edd7d
VojtechStep <vojtechstepancik@outlook.com>
2017-03-26 19:33:05 +0200

    Rewrite to fit the new library

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,53 @@
-#include <RFM69.h>
+#include "../lib/RadioHead/RH_RF69.h"
+#include <Arduino.h>
 
-#define FREQUENCY RF69_433MHZ
-#define LFREQUENCY 434200000
-#define NODEID 100
-#define NETWORKID 42
-#define PROMISCUOUS true
+#define LOBSTER_FREQUENCY 434.200
 
-RFM69 radio(10, 2, true, digitalPinToInterrupt(2));
+RH_RF69 radio(10, 2);
 
-template<typename T>
-	bool TrySuccess(T, T, char* = (char*)"");
+struct
+{
+    uint8_t lat_o;
+    uint8_t lon_o;
+    uint16_t temp;
+    uint16_t pres;
+    uint16_t hmdt;
+    float time;
+    float lat;
+    float lon;
+    float alt;
+    float batlvl;
+    float velocity;
+    int16_t acc[3];
+    int16_t gyro[3];
+    int16_t mag[3];
+    int8_t rssi;
+} data;
 
-void setup() {
-  Serial.begin(9600);
-	Serial.println("Starting");
-	delay(1000);
-  radio.initialize(FREQUENCY, NODEID, NETWORKID);
-  radio.setHighPower(true);
-  radio.setFrequency(LFREQUENCY);
-	radio.promiscuous(PROMISCUOUS);
-	Serial.println("Listening at 433 Mhz");
+void setup()
+{
+    Serial.begin(9600);
+    while(!Serial)
+        ;
+    if (!radio.init())
+        Serial.println("Radio initialization failed");
+    if (!radio.setFrequency(LOBSTER_FREQUENCY))
+        Serial.println("Radio initialization failed");
+    radio.setTxPower(14, true);
+    radio.setEncryptionKey(NULL);
 }
 
-byte ackCount=0;
-uint32_t packetCount = 0;
-void loop() {
-  if(radio.receiveDone()) {
-    Serial.print("#[");
-    Serial.print(++packetCount);
-    Serial.print(']');
-    Serial.print('[');Serial.print(radio.SENDERID, DEC);Serial.print("] ");
-    if (PROMISCUOUS)
+void loop()
+{
+    if (radio.available())
     {
-      Serial.print("to [");Serial.print(radio.TARGETID, DEC);Serial.print("] ");
+        uint8_t len = sizeof(data);
+        if (radio.recv((uint8_t*)&data, &len))
+        {
+            data.rssi = radio.rssiRead();
+            for(uint8_t i = 0; i < len; i++)
+                Serial.write(((uint8_t*)&data)[i]);
+        }
+        else { Serial.println("Receive failed"); }
     }
-    for (byte i = 0; i < radio.DATALEN; i++)
-      Serial.print((char)radio.DATA[i]); //TODO deserialize data or add a parser to js
-    Serial.print("   [RX_RSSI:");Serial.print(radio.RSSI);Serial.print("]");
-
-    if (radio.ACKRequested())
-    {
-      byte theNodeID = radio.SENDERID;
-      radio.sendACK();
-      Serial.print(" - ACK sent.");
-
-      // When a node requests an ACK, respond to the ACK
-      // and also send a packet requesting an ACK (every 3rd one only)
-      // This way both TX/RX NODE functions are tested on 1 end at the GATEWAY
-      if (ackCount++%3==0)
-      {
-        Serial.print(" Pinging node ");
-        Serial.print(theNodeID);
-        Serial.print(" - ACK...");
-        delay(3); //need this when sending right after reception .. ?
-        if (radio.sendWithRetry(theNodeID, "ACK TEST", 8, 0))  // 0 = only 1 attempt, no retries
-          Serial.print("ok!");
-        else Serial.print("nothing");
-      }
-    }
-    Serial.println();
-  }
-  delay(500);
 }
-
-template<typename T>
-	bool TrySuccess(T result, T expect, char* action)
-	{
-		if (result == expect)
-			printf("Success %s\n", action);
-		else
-			printf("Error %s. Error: %d\n", action, result);
-		return result == expect;
-	}
